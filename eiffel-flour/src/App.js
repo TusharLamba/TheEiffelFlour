ce1f4cf484219fb39f32feb8f329ccffa7f0161e
Tushar Lamba <tushar.lamba@hp.com>
2024-04-13 00:29:32 +0530

    Initial Commit

---
+++ eiffel-flour/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './components/Login/index.js';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App;
